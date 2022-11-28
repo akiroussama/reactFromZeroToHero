95acb2b0376f446b1ae2ed632d4e2e64123ce432
oakir <oakir@ecomundo.eu>
2022-11-28 16:00:09 +0100

    Initialize ApolloClient

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { worker } from "./mocks";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
 // Start a mock API server to handle auth requests
 worker.start({
   onUnhandledRequest: "bypass",
 });
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
