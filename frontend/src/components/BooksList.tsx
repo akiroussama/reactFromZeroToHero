0174bc982887780d5b429458854f05ea268e42b9
oakir <oakir@ecomundo.eu>
2022-11-28 16:45:08 +0100

    fix eslint issue

--- frontend/src/components/BooksList.tsx
+++ frontend/src/components/BooksList.tsx
@@ -1,5 +1,6 @@
 // Import everything needed to use the `useQuery` hook
 import { useQuery, gql } from "@apollo/client";
+import { A } from "msw/lib/SetupApi-b2f0e5ac";
 import React from "react";
 
 const GET_BOOKS_LIST = gql`
@@ -23,9 +24,9 @@ const BooksList = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
   console.log(data);
-  return data.books.map((BookData) => (
-    <div key={BookData.id}>
-      <h3>{BookData.title}</h3>
+  return data.books.map((books: BookData) => (
+    <div key={books.id}>
+      <h3>{books.title}</h3>
       <br />
     </div>
   ));
