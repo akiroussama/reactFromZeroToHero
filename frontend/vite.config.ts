4a3f15c4a55a463fe531a31f7e031e77e485cf9e
oakir <oakir@ecomundo.eu>
2022-12-05 04:53:47 +0100

    dockerise frontend & backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// fix port 3000
+const port = 3000;
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [react()],
+	server: {
+		hmr: {
+			host: "0.0.0.0",
+			port: 3000,
+		},
+		watch: {
+			usePolling: true,
+		},
+		host: true,
+		port: port,
+	},
+});
